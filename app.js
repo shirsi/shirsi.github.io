a5f585f9050f91542592d9c86c526e905cabc439
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-02-11 18:17:50 -0600

    favor buttons work

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ $(() => {
 
 ////////////////////////////////////////////////////////////////////
 // Search Function
-// ////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////
+
+
+
+console.log("hey");
+
+
 $('.button').on('click', (event) => {
+
+
   event.preventDefault()
     $(".characters").empty()
   console.log("whats");
@@ -36,14 +44,14 @@ for (i = 0; i< data.results.length; i++){
     .text(`Full name: ${data.results[i].biography['full-name']}`)
     .appendTo($infodiv)
   // for (p = 0; x< data.results[i].powerstats.length; x++){
-    let $powerstats = $('<ul>').appendTo($infodiv).addClass('powerstats').text("Power Stats")
+    let $powerstats = $('<ul>').appendTo($infodiv).addClass('powerstats').text("Power Stats: ")
     let intelligence = $('<li>')
           .text(`Intelligence: ${data.results[i].powerstats.intelligence}`)
           .appendTo($powerstats)
 
           console.log(data.results[i].powerstats);
 // }
-    const $ul =$('<ul>').text("Aliases")
+    const $ul =$('<ul>').text("Aliases: ")
     $infodiv.append($ul)
       for (x = 0; x< data.results[i].biography.aliases.length; x++){
           let $li = $('<li>')
@@ -68,11 +76,11 @@ let input = 1
 // console.log(link);
 
 
-for (i= 1; i<=10;i++){
-input =+ i
+for (i= 1; i<=14;i++){
+input =+i
 let link = `https://www.superheroapi.com/api.php/10222119373912674/${input}`
 // console.log(link);
-// console.log(userInput);
+// console.log(input);
 $.ajax({
   url:link ,
   type: "GET"
@@ -80,18 +88,18 @@ $.ajax({
 }).then((data)=>{
   console.log(data);
   const $img = $("<img />").attr('src' , data.image.url).appendTo('.photos')
-console.log($img);
+// console.log($img);
 
 
 
-},  (()=>{
-
+},  ((word)=>{
+console.log(word);
 console.log("Error")
 }))
 
 
 let currentImgIndex = 0
-let highestIndex = 9
+let highestIndex = 13
 $('.next').on('click', () => {
   event.preventDefault()
 $('.photos').children().eq(currentImgIndex).css('display', 'none');
@@ -115,6 +123,63 @@ if (currentImgIndex < highestIndex ) {
 })
 
 }
+$('.hero').on('click',(event) => {
+  event.preventDefault()
+  $(".favoriteCharactersInfo").empty()
 
+  let subject = $(event.target).attr('id')
+  console.log(event.target);
+  let link = `https://www.superheroapi.com/api.php/10222119373912674/${subject}`
+  console.log(link);
+
+
+
+  $.ajax({
+    url:link,
+    type: "GET",
+    data: {
+        "$limit": 731
+      }
+
+  }).then((data)=>{
+
+console.log(data);
+{
+  const $div = $('<div>').appendTo(".favoriteCharacters")
+  const $infodiv =$('<div>').appendTo($div).addClass('.favoriteCharactersInfo')
+  const $h2 = $('<h2>')
+    .text(`${data.name}`)
+          .appendTo($infodiv)
+
+  const $name = $('<h3>')
+    .text(`Full name: ${data.biography['full-name']}`)
+    .appendTo($infodiv)
+  // for (p = 0; x< data.results[i].powerstats.length; x++){
+    let $powerstats = $('<ul>').appendTo($infodiv).addClass('powerstats').text("Power Stats: ")
+    let intelligence = $('<li>')
+          .text(`Intelligence: ${data.powerstats.intelligence}`)
+          .appendTo($powerstats)
+
+          console.log(data.powerstats);
+  // }
+    const $ul =$('<ul>').text("Aliases: ")
+    $infodiv.append($ul)
+      for (x = 0; x< data.biography.aliases.length; x++){
+          let $li = $('<li>')
+                  .text(`${data.biography.aliases[x]}`)
+                  .appendTo($ul)
+                }
+      const $img = $("<img />").attr('src' ,`${data.image.url}`)
+        $div.append($img)
+
+  }
+  })
+  //
 })
+
+  });
+
+
+
+
 // 10222119373912674
